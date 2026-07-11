2975ec988f875e5287ad960836dd834854d798d6
Antônio Sales <antonio.saleses@gmail.com>
2026-07-11 17:39:02 -0300

    Estudo de condicionais

---
+++ javaCourse/_02_CondicionalIf.java
@@ -0,0 +1,11 @@
+public class _02_SouHumano {
+    public static void main(String[] args) {
+        boolean isHuman = true;
+
+        if (isHuman) {
+            System.out.println("Hello! Welcome to the real world.");
+        } else {
+            System.out.println("Ow, you are not real!");
+        }
+    }
+}
