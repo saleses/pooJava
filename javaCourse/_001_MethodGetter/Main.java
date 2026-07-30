ac46af531e249bc6af5c1c083086c50814037b91
Antônio Sales <antonio.saleses@gmail.com>
2026-07-30 17:06:40 -0300

    Alteração em código

--- javaCourse/_001_MethodGetter/Main.java
+++ javaCourse/_001_MethodGetter/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        Car eletric = new Car();
+        Car eletricCar = new Car();
 
         System.out.println("Método Getter: acesso a atributos privados (private)");
-        System.out.println("make = " + eletric.getMake());
-        System.out.println("model = " + eletric.getModel());
-        System.out.println("doors = " + eletric.getDoors());
-        System.out.println("color = " + eletric.getColor());
-        System.out.println("Convertible = " +  eletric.isConvertible());
+        System.out.println("make = " + eletricCar.getMake());
+        System.out.println("model = " + eletricCar.getModel());
+        System.out.println("doors = " + eletricCar.getDoors());
+        System.out.println("color = " + eletricCar.getColor());
+        System.out.println("Convertible = " +  eletricCar.isConvertible());
         System.out.println("-------------------------------------");
         System.out.println("Método public: acesso a atributos através de um método público");
-        eletric.describeCar();
+        eletricCar.describeCar();
     }
 }
