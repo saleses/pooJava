3414ef648123419fb485f7ebd3d06066520b7d0d
Antônio Sales <antonio.saleses@gmail.com>
2026-08-10 22:33:53 -0300

    Estudo de técnicas de Encapsulamento

---
+++ javaCourse/_010_Encapsulation/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Printer printer = new Printer(50, true);
+        System.out.println("Initial pages count = " + printer.getPagesPrinted());
+
+        int pagesPrinted = printer.printPages(5);
+        System.out.printf("Current Job Pages: %d, Printer Total: %d %n", pagesPrinted, printer.getPagesPrinted());
+    }
+}
